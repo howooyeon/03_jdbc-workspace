962254e789339a7e06364fc3cc5db288f651c0d5
potato <aviationlovers@naver.com>
2023-07-07 16:33:18 +0900

    commit

---
+++ Sample Script.sql
@@ -0,0 +1,56 @@
+-- TABLE DROP
+DROP TABLE MEMBER;
+-- SEQUENCE DROP
+DROP SEQUENCE SEQ_USERNO;
+
+CREATE TABLE MEMBER (
+    USERNO NUMBER PRIMARY KEY,                  -- 회원번호
+    USERID VARCHAR2(15) NOT NULL UNIQUE,        -- 회원아이디
+    USERPWD VARCHAR2(15) NOT NULL,              -- 회원비번
+    USERNAME VARCHAR2(20) NOT NULL,             -- 회원이름
+    GENDER CHAR(1) CHECK(GENDER IN ('M', 'F')), -- 성별
+    AGE NUMBER,                                 -- 나이
+    EMAIL VARCHAR2(30),                         -- 이메일
+    PHONE CHAR(11), -- 짝대기없는거               -- 전번
+    ADDRESS VARCHAR2(100),                      -- 주소
+    HOBBY VARCHAR2(50),                         -- 취미
+    ENROLLDATE DATE DEFAULT SYSDATE NOT NULL    -- 회원가입일
+);
+
+SELECT * FROM MEMBER;
+
+CREATE SEQUENCE SEQ_USERNO
+NOCACHE;
+
+INSERT INTO MEMBER
+VALUES(SEQ_USERNO.NEXTVAL, 'admin', '1234', '관리자', 'M', 45, 'admin@iei.or.kr', '01012345555', '서울', NULL, '2021/07/07');
+
+INSERT INTO MEMBER
+VALUES(SEQ_USERNO.NEXTVAL, 'user01', 'pass01', '홍길동', null, 23, 'user01@iei.or.kr', '01022221111', '부산', '등산, 영화보기', '2023/03/02');
+
+COMMIT;
+
+CREATE TABLE TEST(
+    TNO NUMBER,
+    TNAME VARCHAR2(20),
+    TDATE DATE
+);
+
+SELECT * FROM TEST;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
