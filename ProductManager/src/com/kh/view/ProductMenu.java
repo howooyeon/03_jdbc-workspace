e1dffaa78213552b5492acda770a89b499c09734
potato <aviationlovers@naver.com>
2023-07-14 17:34:21 +0900

    commit

--- ProductManager/src/com/kh/view/ProductMenu.java
+++ ProductManager/src/com/kh/view/ProductMenu.java
@@ -12,7 +12,7 @@ public class ProductMenu {
 	public void mainMenu() {
 		
 		while(true) {
-			System.out.println("1. 전체조회하기");
+			System.out.println("1. 전체 조회 하기");
 			System.out.println("2. 상품 추가 하기");
 			System.out.println("3. 상품 수정 하기 (상품 id로 조회하고 수정)");
 			System.out.println("4. 상품 삭제 하기 (상품 id로 조회해서 삭제)");
